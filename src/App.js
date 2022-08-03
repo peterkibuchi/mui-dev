9d8904a1c0e076c6cb00dfece6b25e58c45d834c
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2022-08-03 06:54:37 +0300

    Create Layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Box, Stack } from "@mui/material";
+import Feed from "./components/Feed/Feed";
+import Navbar from "./components/Navbar/Navbar";
+import Rightbar from "./components/Rightbar/Rightbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+
 function App() {
   return (
-    <h1>
-      App
-    </h1>
+    <Box>
+      <Navbar />
+      <Stack direction="row" spacing={2} justifyContent="space-around">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
   );
 }
 
