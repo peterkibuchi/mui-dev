593cb6569c00850ae91aed5897cc734d035f16f6
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2022-08-03 06:55:14 +0300

    Create Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,103 @@
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import StyleIcon from "@mui/icons-material/Style";
+import { Mail, Notifications } from "@mui/icons-material";
+import { useState } from "react";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  gap: "20px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "10px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          Mui Dev
+        </Typography>
+        <StyleIcon sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            alt="Cindy Baker"
+            src="https://images.pexels.com/photos/1102341/pexels-photo-1102341.jpeg?cs=srgb&dl=pexels-daniel-xavier-1102341.jpg&fm=jpg"
+            onClick={(e) => setOpen(!open)}
+          />
+        </Icons>
+        <UserBox onClick={(e) => setOpen(!open)}>
+          <Typography variant="span">Stacy</Typography>
+          <Avatar
+            alt="Cindy Baker"
+            src="https://images.pexels.com/photos/1102341/pexels-photo-1102341.jpeg?cs=srgb&dl=pexels-daniel-xavier-1102341.jpg&fm=jpg"
+          />
+        </UserBox>
+      </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  );
+};
+
+export default Navbar;
